2922b6b763b1b4ead31af6726057a6553ea01d5b
Jorge Mario Cruz-Duarte <jorge.cruz@itesm.mx>
2019-09-18 19:29:12 -0500

    alpha version
    to attend: how to call methods inside a class using strings... there would be something like "eval(__string__)" in MATLAB... look for that!

--- metaheuristics.py
+++ metaheuristics.py
@@ -5,34 +5,57 @@ Created on Sun Sep 15 16:35:32 2019
 
 @author: jkpvsz
 """
+# Packages
+from population import Population 
+from opteval import benchmark_func as bf
 
-from operatos import *
-
-def RandomSearch(problemFunction=Jong, nbDimensions=2, nbPopulation=25, nbIterations=100):
-    # Initialisation phase
-    population = uniformInitialisation(nbDimensions, nbPopulation)
+def run():
+    # Problem definition
+    num_dimensions = 2
+    num_agents = 30
+    num_iterations = 100
+    problem = bf.Sphere(num_dimensions)
+    is_constrained = True
+    desired_fitness = 1E-6
     
-    # Evaluate current population
-    fitness = problemEvaluation(problemFunction,population)
+    # Call optimisation method
+    RandomSearch(problem, num_dimensions, num_agents, num_iterations, 
+                 desired_fitness, is_constrained)
     
-    # Find the best agent and its fitness
-    bestAgent, bestFitness = findBest(population, fitness)
+def RandomSearch(problem, num_dimensions = 2, num_agents = 30, 
+                     num_iterations = 100, desired_fitness = 1E-6, 
+                     is_constrained = True):
+    
+    # Create population
+    population = Population(problem,desired_fitness,num_agents,is_constrained)
+    
+    # Initialise population with random elements uniformly distributed in space
+    population.initialise_uniformly()
+    
+    # Evaluate fitness values
+    population.evaluate_fitness()
+    
+    # Update population, global, etc
+    population.update_population("all")
+    population.update_particular("greedy")
+    population.update_global("greedy")
     
     # TODO initialise historical data
     
-    for iteration in range(1,nbIterations+1):
-        # Determine candidate population by perturbating current population
-        new_population = simpleRandomWalk(population, scale = 0.0001)
+    # Start optimisaton procedure
+    for iteration in range(1,num_dimensions + 1):
+        # Perform a perturbation
+        population.random_walk(0.01)
         
-        # Evaluate this cantidate population
-        new_fitness = problemEvaluation(problemFunction,population)
-        
-        # Update current population and its fitness
-        population, fitness = greedySelector(population, fitness, new_population, new_fitness)
-        
-        # Update the best agent and its fitness
-        bestAgent, bestFitness = findBest(population, fitness, bestAgent, bestFitness)
+        # Evaluate fitness values
+        population.evaluate_fitness()
+    
+        # Update population, global, etc
+        population.update_population("all")
+        population.update_particular("greedy")
+        population.update_global("greedy")
         
         # Print information per iteration
-        print(f"[{iteration}]\t-> {bestFitness}")
+        status = population.get_state()
+        print(f"[{iteration}]\t-> {status}")
     
--- population.py
+++ population.py
@@ -8,8 +8,9 @@ import numpy as np
 
 class Population():
 
-    def __init__(self, problem, desired_fitness, num_agents):
-        # Read problem (it must be from from opteval import benchmark_func as bf)
+    def __init__(self, problem, desired_fitness = 1E-6, num_agents = 30, 
+                 is_constrained = True):
+        # Read problem (from opteval import benchmark_func as bf)
         self.problem = problem
         
         # Read desired fitness to achieve
@@ -22,20 +23,21 @@ class Population():
         self.num_dimensions = problem.variable_num
         
         # Read the upper and lower boundaries of search space
-        self.lowerBoundaries = problem.min_search_range
-        self.upperBoundaries = problem.max_search_range
+        self.lower_boundaries = problem.min_search_range
+        self.upper_boundaries = problem.max_search_range
         
         # Initialise positions and fitness values
         self.positions = np.full((self.num_agents,self.num_dimensions),np.nan)
         self.fitness = np.full((self.num_agents,1),np.nan)
         
-        # Initialise additional parameters (those corresponding to other algorithms)
+        # Initialise additional params (those corresponding to other algs)
         # -> Velocities for PSO-based search methods
-        self.velocities = np.full((self.num_agents,self.num_dimensions),np.nan) 
+        self.velocities = np.full((self.num_agents,self.num_dimensions),0) 
         
         # General fitness measurements
         self.global_best_fitness = float('inf')
-        self.global_best_position = np.random.uniform(-1,1,(self.num_agents,self.num_dimensions))
+        self.global_best_position = np.random.uniform(-1, 1, 
+                                        (self.num_agents, self.num_dimensions))
     
         self.particular_best_fitness = self.fitness
         self.particular_best_position = self.positions
@@ -43,23 +45,99 @@ class Population():
         self.previous_fitness = self.fitness
         self.previous_position = self.positions
         
+        self.is_constrained = is_constrained
+        
         # TODO Add capábility for dealing with topologies (neighbourhoods)
         # self.local_best_fitness = self.fitness
         # self.local_best_positions = self.positions
+    
+    # [E] Generate a string containing the current state of the population
+    def get_state(self):
+        print("x_best = ", self.global_best_position, " with f_best = ", 
+              self.global_best_fitness)
+    
+    # -------------------------------------------------------------------------
+    # 1. Initialisators
+    # -------------------------------------------------------------------------
+    
+    # [E] 1.1. Initialise population using a random uniform distribution in [-1,1]]
+    def initialise_uniformly(self):
+        for agent in range(0, self.num_agents):
+            self.positions[agent] = np.random.uniform(-1, 1, 
+                                      self.num_dimensions)   
+    
+    #TODO Add more initialisation operators like grid, boundary, etc.
+    
+    # -------------------------------------------------------------------------
+    # 2. Perturbators
+    # -------------------------------------------------------------------------
+    
+    # [E] 2.1. Random Walk    
+    def random_walk(self, scale = 0.01, *args):
+        for agent in range(0, self.num_agents):
+            self.positions[agent,:] += scale * np.random.uniform(-1, 1, 
+                                          self.num_dimensions)
+            if self.is_constrained: self.check_simple_constraints(agent)
+    
+    # [E] 2.2. Inertial PSO movement
+    def inertial_pso(self, w = 0.7, phi_1 = 2.52, phi_2 = 2.53, *args):
+        for agent in range(0, self.num_agents):
+            r_1 = np.random.rand(self.num_dimensions)
+            r_2 = np.random.rand(self.num_dimensions)
+            self.velocities[agent,:] = w*self.velocities[agent,:] + \
+                phi_1 * r_1 * (self.positions[agent,:] - \
+                self.particular_best_position[agent,:]) + phi_2 * r_2 * \
+                (self.positions[agent,:] - self.global_best_position)
+            self.positions[agent,:] = self.positions[agent,:] + \
+                self.velocities[agent,:]            
+            if self.is_constrained: self.check_simple_constraints(agent)
+    
+    # [E] 2.3. Constricted PSO movement                
+    def constricted_pso(self, chi = 0.7, phi_1 = 2.52, phi_2 = 2.53, *args):
+        for agent in range(0, self.num_agents):
+            r_1 = np.random.rand(self.num_dimensions)
+            r_2 = np.random.rand(self.num_dimensions)
+            self.velocities[agent,:] = chi*(self.velocities[agent,:] + \
+                phi_1 * r_1 * (self.positions[agent,:] - \
+                self.particular_best_position[agent,:]) + phi_2 * r_2 * \
+                (self.positions[agent,:] - self.global_best_position))
+            self.positions[agent,:] = self.positions[agent,:] + \
+                self.velocities[agent,:]            
+            if self.is_constrained: self.check_simple_constraints(agent)
+
+    # -------------------------------------------------------------------------
+    # 3. Basic methods
+    # -------------------------------------------------------------------------
+    
+    # 3.1. Check simple contraints if self.is_constrained = True
+    def check_simple_constraints(self, agent):
         
-    # Rescale an agent from [-1,1] to [lower,upper] per dimension
+        low_check = self.positions[agent,:] < self.lower_boundaries
+        if low_check.any():
+            self.positions[agent, low_check] = self.lower_boundaries[low_check]
+            self.velocities[agent, low_check] = 0.
+            
+        upp_check = self.positions[agent,:] > self.upper_boundaries
+        if upp_check.any():
+            self.positions[agent, upp_check] = self.upper_boundaries[upp_check]
+            self.velocities[agent, upp_check] = 0.
+        
+    # 3.2. Rescale an agent from [-1,1] to [lower,upper] per dimension
     def rescale_back(self,agent):
-    # y = ((b - a) * x + (a + b)) / 2
-        return ((self.upperBoundaries + self.lowerBoundaries) + agent * (self.upperBoundaries - self.lowerBoundaries)) / 2
+        return ((self.upper_boundaries + self.lower_boundaries) + agent * \
+                (self.upper_boundaries - self.lower_boundaries)) / 2
 
-
+    # [E] 3.3. Evaluate population positions in the problem function
     def evaluate_fitness(self):
         for agent in range(0, self.num_agents):
-            self.fitness[agent] = self.problem(self.rescale_back(self.positions[agent,:]))
+            self.fitness[agent] = self.problem.get_func_val(
+                    self.rescale_back(self.positions[agent,:]))
     
-    def update_population(self,selection_method="greedy"):
+    # [E] 3.4. Update population positions according to a selection scheme
+    def update_population(self, selection_method = "all"):
         for agent in range(0,self.num_agents):
-            if locals()[selection_method](self.fitness[agent], self.previous_fitness[agent]):
+            if locals()[selection_method+"_selection"](self.fitness[agent], 
+                     self.previous_fitness[agent]):
                 # if new positions are improved, then update past register ...
                 self.previous_fitness[agent] = self.fitness[agent]
                 self.previous_position[agent,:] = self.positions[agent,:]
@@ -68,54 +146,46 @@ class Population():
                 self.fitness[agent] = self.previous_fitness[agent]
                 self.positions[agent,:] = self.previous_position[agent,:]
 
-    def update_particular(self,selection_method="greedy"):        
+    # [E] 3.5. Update particular positions acording to a selection scheme
+    def update_particular(self, selection_method = "greedy"):        
         for agent in range(0,self.num_agents):
-            if locals()[selection_method](self.fitness[agent], self.particular_best_fitness[agent]):
+            if locals()[selection_method+"_selection"](self.fitness[agent], 
+                     self.particular_best_fitness[agent]):
                 self.particular_best_fitness[agent] = self.fitness[agent]
-                self.particular_best_position[agent,:] = self.positions[agent,:]
+                self.particular_best_position[agent,:] =self.positions[agent,:]
     
-#    def greedySelector(population, fitness, new_population, new_fitness):
-#        # TODO improve by using np.ndarray.min(,axis) and np.ndarray.argmin(,axis)
-#        for agent in range(0,population.shape[1]):
-#            if new_fitness[agent] <= fitness[agent]:
-#                fitness[agent] = new_fitness[agent]
-#                population[:,agent] = new_population[:,agent]
-#        return population, fitness
-    
-    def update_global(self,selection_method="greedy"):        
+    # 3.6. [E] Update global position according to a selection scheme
+    def update_global(self, selection_method = "greedy"):        
         # Read current global best agent
         current_global_best_position = self.positions[self.fitness.argmin(),:]
         current_global_best_fitness = self.fitness.min()
-        
-        # Check if global best is improved
-        #if current_global_best_fitness < self.global_best_fitness:
-        if locals()[selection_method](current_global_best_fitness, self.global_best_fitness):
+        if locals()[selection_method+"_selection"](current_global_best_fitness, 
+                 self.global_best_fitness):
             self.particular_best_position = current_global_best_position
             self.current_global_best_fitness = current_global_best_fitness 
     
-    # Selection methods
+    # -------------------------------------------------------------------------
+    # 4. Selector methods
+    # -------------------------------------------------------------------------
     
-    def greedy_selection(new,old,*args):
+    def greedy_selection(new, old, *args):
         return new < old
     
-    def none_selection(new,old,*args):
+    def none_selection(new, old, *args):
         return False
     
-    def metropolis_selection(new,old,*args):
-        # args[0] - initial temperature (T_0)
-        # args[1] - cooling rate (c)
-        # args[2] - current iteration (it | n)
-        #//print(f"T_0 = {args[0]}, c = {args[1]}, n = {args[2]},")
-        energies_difference = new - old
-        if energies_difference <= 0:
-            selection_conditon = True
+    def all_selection(new, old, *args):
+        return True
+    
+    def metropolis_selection(new, old, T_0=1000, c=0.01, n=1, *args):
+        # T_0 - initial temperature (T_0)
+        # c - cooling rate (c)
+        # n - current iteration (it | n)
+        if new <= old: selection_conditon = True
         else:
-            current_temperature = args[0]*(1 - args[1])**args[2]
-            boltzmann_probability = np.math.exp(-energies_difference/current_temperature)
-            if boltzmann_probability > np.random.rand(): 
+            if np.math.exp(-(new - old)/(T_0*(1 - c)**n)) > np.random.rand(): 
                 selection_conditon = True
-            else:
-                selection_conditon = False
+            else: selection_conditon = False
         return selection_conditon 
         
     
