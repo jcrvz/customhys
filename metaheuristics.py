dbb51369f0a58e6513d3ea106fb8c7c3abd48180
Jorge Mario Cruz-Duarte <jorge.cruz@itesm.mx>
2019-09-20 17:20:15 -0500

    new improvements and additions

--- metaheuristics.py
+++ metaheuristics.py
@@ -16,7 +16,8 @@ import matplotlib.pyplot as plt
 num_dimensions = 2
 num_agents = 50
 num_iterations = 100
-problem = bf.Sphere(num_dimensions)
+#problem = bf.Sphere(num_dimensions)
+problem = bf.Rosenbrock(num_dimensions)
 is_constrained = True
 desired_fitness = 1E-6
 
@@ -58,28 +59,37 @@ pop.update_global("greedy")
 
 # TODO initialise historical data
 
+pop.metropolis_temperature = 10000
+pop.metropolis_rate = 1
+
 # Start optimisaton procedure
 for iteration in range(1, num_iterations + 1):
     # Perform a perturbation
-#    pop.random_walk(0.1)
+#    pop.random_search()
+#    pop.random_sample()
+#    pop.rayleigh_search()
 #    pop.constricted_pso()
 #    pop.inertial_pso()
-    pop.levy_flight()
-    
-    # -- plot population
-    plt.plot(pop.positions[:,0],pop.positions[:,1],'ro')
-    plt.xlabel('x_1')
-    plt.ylabel('x_2')
-    plt.axis([-1.1,1.1,-1.1,1.1])
-    plt.pause(0.01)
-    plt.draw()
-    
+#    pop.levy_flight()  
+        
     # Evaluate fitness values
     pop.evaluate_fitness()
 
     # Update population, global
-    pop.update_population("greedy")
-    pop.update_global("greedy")
+    pop.iteration = iteration
+    pop.update_population("all")
+    pop.update_global()
+    
+    # -- plot population
+    plt.cla()
+    plt.plot(pop.positions[:,0],pop.positions[:,1],'ro')
+    plt.plot(pop.global_best_position[0],pop.global_best_position[1],'bo')
+    plt.xlabel('x_1')
+    plt.ylabel('x_2')
+    plt.title(str(pop.global_best_fitness)+" at "+str(iteration))
+    plt.axis([-1.1,1.1,-1.1,1.1])
+    plt.pause(0.01)
+    plt.draw()
     
     # Print information per iteration
     if (iteration % 10) == 0:
--- population.py
+++ population.py
@@ -6,10 +6,32 @@ Created on Tue Sep 17 14:29:43 2019
 """
 import numpy as np
 
+__operators__ = ['random_search', 'random_sample', 'rayleigh_flight', 'inertial_pso',
+                 'constricted_pso','levy_flight']
+__selection__ = ['all','none','greedy','metropolis']
+
 class Population():
+    
+    # Internal variables
+    iteration = 1
+    
+    # Parameters per selection method    
+    metropolis_temperature = 1000
+    metropolis_rate = 0.01
+    
+    # Parameters per perturbation operator
+    pso_self_confidence = 2.25
+    pso_swarm_confidence = 2.52
+    pso_kappa = 1.0
+    pso_inertial = 0.8
+    
+    levy_alpha = 1.0
+    levy_beta = 1.5
+    
+    random_scale = 0.01
 
     def __init__(self, problem, desired_fitness = 1E-6, num_agents = 30, 
-                 is_constrained = True):
+                 is_constrained = True):        
         # Read problem (from opteval import benchmark_func as bf)
         self.problem = problem
         
@@ -80,44 +102,59 @@ class Population():
     # 2. Perturbators
     # -------------------------------------------------------------------------
     
-    # [E] 2.1. Random Walk    
-    def random_walk(self, scale = 0.01, *args):
-        self.positions += scale * np.random.uniform(-1,1,(self.num_agents, 
+    # [E] 2.1. Random sample  
+    def random_sample(self):
+        self.positions = np.random.uniform(-1,1,(self.num_agents, 
             self.num_dimensions))
-        self.is_constrained: self.check_simple_constraints()
+        if self.is_constrained: self.check_simple_constraints()
+    
+    # [E] 2.1. Random Walk    
+    def random_search(self):
+        self.positions += self.random_scale * np.random.uniform(-1,1,
+            (self.num_agents, self.num_dimensions))
+        if self.is_constrained: self.check_simple_constraints()    
+    
+    def rayleigh_flight(self):
+        self.positions += self.random_scale * np.random.standard_normal(
+            (self.num_agents, self.num_dimensions))
+        if self.is_constrained: self.check_simple_constraints()
     
     # [E] 2.2. Inertial PSO movement
-    def inertial_pso(self, w = 0.7, c_p = 2.25, c_g = 2.52, *args):        
-        r_1 = np.random.uniform(0,1,(self.num_agents, self.num_dimensions))
-        r_2 = np.random.uniform(0,1,(self.num_agents, self.num_dimensions))
+    def inertial_pso(self):        
+        r_1 = self.pso_self_confidence * \
+                np.random.uniform(0,1,(self.num_agents, self.num_dimensions))
+        r_2 = self.pso_swarm_confidence * \
+                np.random.uniform(0,1,(self.num_agents, self.num_dimensions))
         
-        self.velocities = w * self.velocities + c_p * r_1 * \
-                (self.particular_best_positions - self.positions) + c_g * \
-                r_2 * (np.tile(self.global_best_position, 
-                (self.num_agents, 1)) - self.positions)
+        self.velocities = self.pso_inertial * self.velocities + r_1 * \
+                (self.particular_best_positions - self.positions) + r_2 * \
+                (np.tile(self.global_best_position, (self.num_agents, 1)) - \
+                 self.positions)
         self.positions += self.velocities
         if self.is_constrained: self.check_simple_constraints()
     
     # [E] 2.3. Constricted PSO movement                
-    def constricted_pso(self, c_p = 2.25, c_g = 2.52, kappa = 1.0, *args):
-        phi = c_p + c_g
-        chi = 2 * kappa / np.abs(2 - phi - np.sqrt(phi**2 - 4*phi))
-        r_1 = np.random.uniform(0,1,(self.num_agents, self.num_dimensions))
-        r_2 = np.random.uniform(0,1,(self.num_agents, self.num_dimensions))
+    def constricted_pso(self):
+        phi = self.pso_self_confidence + self.pso_swarm_confidence
+        chi = 2 * self.pso_kappa / np.abs(2 - phi - np.sqrt(phi**2 - 4*phi))
+        r_1 = self.pso_self_confidence * \
+                np.random.uniform(0,1,(self.num_agents, self.num_dimensions))
+        r_2 = self.pso_swarm_confidence * \
+                np.random.uniform(0,1,(self.num_agents, self.num_dimensions))
         
-        self.velocities = chi*(self.velocities + c_p * r_1 * \
-                (self.particular_best_positions - self.positions) + c_g * \
-                r_2 * (np.tile(self.global_best_position, 
-                (self.num_agents, 1)) - self.positions))
+        self.velocities = chi *(self.velocities + r_1 * \
+                (self.particular_best_positions - self.positions) + r_2 * \
+                (np.tile(self.global_best_position, (self.num_agents, 1)) - \
+                 self.positions))
         self.positions += self.velocities
         if self.is_constrained: self.check_simple_constraints()
         
     # [E] 2.4. Lévy flight from Cuckoo Search
-    def levy_flight(self, alpha = 1.0, beta = 1.5, *args):
+    def levy_flight(self):
         # Calculate std dev of u (Mantegna's algorithm)
-        sigma = ((np.math.gamma(1 + beta) * np.sin(np.pi*beta/2)) \
-                / (np.math.gamma((1 + beta)/2)*beta* (2**((beta - 1)/2)))) \
-                ** (1/beta)
+        sigma = ((np.math.gamma(1 + self.levy_beta) * np.sin(np.pi*
+            self.levy_beta/2)) / (np.math.gamma((1 + self.levy_beta)/2)*\
+            self.levy_beta*(2**((self.levy_beta - 1)/2))))** (1/self.levy_beta)
         
         x = sigma * np.random.standard_normal((self.num_agents, 
                 self.num_dimensions))
@@ -125,14 +162,11 @@ class Population():
                 self.num_dimensions)))
         z = np.random.standard_normal((self.num_agents, self.num_dimensions))
         
-        levy = x / (y ** (1/beta))
+        levy = x / (y ** (1/self.levy_beta))
         
-        self.positions += alpha * z * levy * (self.positions - \
+        self.positions += self.levy_alpha * z * levy * (self.positions - \
                 np.tile(self.global_best_position, (self.num_agents, 1)))
         
-#        CurrentPopulation = CurrentPopulation + ...
-#            ScaleFactor*valsU.*LevyRand.*...
-#            (CurrentPopulation - repmat(bestAgent,1,PopulationSize)); 
                 
     # -------------------------------------------------------------------------
     # 3. Basic methods
@@ -163,10 +197,10 @@ class Population():
                     self.rescale_back(self.positions[agent,:]))
     
     # [E] 3.4. Update population positions according to a selection scheme
-    def update_population(self, selection_method = "all", *args):
+    def update_population(self, selection_method = "all"):
         for agent in range(0,self.num_agents):
             if getattr(self,selection_method+"_selection")(self.fitness[agent], 
-                     self.previous_fitness[agent], args):
+                     self.previous_fitness[agent]):
                 # if new positions are improved, then update past register ...
                 self.previous_fitness[agent] = self.fitness[agent]
                 self.previous_positions[agent,:] = self.positions[agent,:]
@@ -178,30 +212,30 @@ class Population():
                 self.velocities[agent,:] = self.previous_velocities[agent,:]
 
     # [E] 3.5. Update particular positions acording to a selection scheme
-    def update_particular(self, selection_method = "greedy", *args):        
+    def update_particular(self, selection_method = "greedy"):        
         for agent in range(0, self.num_agents):            
             if getattr(self,selection_method+"_selection")(self.fitness[agent], 
-                     self.particular_best_fitness[agent], args):
+                     self.particular_best_fitness[agent]):
                 self.particular_best_fitness[agent] = self.fitness[agent]
                 self.particular_best_positions[agent,:]=self.positions[agent,:]
     
     # 3.6. [E] Update global position according to a selection scheme
-    def update_global(self, selection_method = "greedy", *args):       
+    def update_global(self, selection_method = "greedy"):       
         self.update_particular(selection_method)
         # Read current global best agent
         candidate_position = self.particular_best_positions[\
                 self.particular_best_fitness.argmin(),:]
         candidate_fitness = self.particular_best_fitness.min()
         if getattr(self,selection_method+"_selection")(candidate_fitness, 
-                  self.global_best_fitness, args) or np.isinf(candidate_fitness):
+                  self.global_best_fitness) or np.isinf(candidate_fitness):
             self.global_best_position = candidate_position
             self.global_best_fitness = candidate_fitness 
     
     # -------------------------------------------------------------------------
     # 4. Selector methods
     # -------------------------------------------------------------------------
-    
-    def greedy_selection(self,new, old, *args):
+
+    def greedy_selection(self, new, old):
         return new <= old
     
     def none_selection(self, *args):
@@ -210,18 +244,15 @@ class Population():
     def all_selection(self, *args):
         return True
     
-    def metropolis_selection(self, new, old, n=1, T_0=1000, c=0.01, *args):
-        # n - current iteration (it | n)
-        # T_0 - initial temperature (T_0)
-        # c - cooling rate (c)        
+    def metropolis_selection(self, new, old):
+        # It depends of metropolis_temperature, metropolis_rate, and iteration
         if new <= old: selection_conditon = True
         else:
-            if np.math.exp(-(new - old)/(T_0*(1 - c)**n)) > np.random.rand(): 
+            if np.math.exp(-(new - old)/(self.metropolis_temperature*\
+                (1 - self.metropolis_rate)**self.iteration))> np.random.rand(): 
                 selection_conditon = True
             else: selection_conditon = False
         return selection_conditon 
-        
-    
     
     
         
