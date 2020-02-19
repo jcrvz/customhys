+ operators.py: - Bug in the rotation matrix was fixed. - Ranges for the automatic hyper-parameter sampling per search operator were revisited. - _obtain_operators was forced to start collection files always with random_search. Now   hyperheuristic.py begings the search procedure from the simplest and general   metaheuristic (cardinality=1, operator=random_search). - Some minor changes were done.
+ benchmark_func.py:
- Sort __all__ list
- Modify "W-Wavy" function to avoid errors when saving figures
- save_fig() method was tested
- __name__ == '__main__' was deleted

+ hyperheuristic.py:
- Default value for parameters input argument was reallocated and it was modified.
- Solver was changed: Random Search -> Simulated Annealing. Therefore, several changes
- corresponds to that.
- Hyper-parameters were modified according to the new solver implemented.
- Cardinality now can have the following forms:
... cardinality = 2      # A fixed cardinality when it is an integer
... cardinality = [2]    # A variable cardinality when it is a 1-element list.
                         # It is equivalent to say [1, 2]
... cardinality = [1,2]  # Similar to the aforementioned case.
  So, in this version cardinality is an additional design variable.

+ test_benchmark_func.py:
- It plots and saves all the functions for testing purposes.

TODO: Test hyperheuristic.py

benchmark_func.py
@@ -33,23 +33,22 @@ import mpl_toolkits.mplot3d
 from matplotlib.colors import LightSource
 # from functools import wraps
 
-__all__ = ['Ackley1', 'Ackley4', 'Alpine1', 'Alpine2', 'Brown', 'Giunta',
-           'ChungReynolds', 'Csendes', 'Deb1', 'Deb3', 'DixonPrice', 'DropWave',
-           'EggHolder', 'Ellipsoid', 'Exponential', 'Griewank', 'HyperEllipsoid',
-           'KTablet', 'Michalewicz', 'Mishra1', 'Mishra11', 'Mishra2', 'Qing',
-           'Mishra7', 'Pathological', 'Perm', 'Pinter', 'PowellSum', 'Brent',
-           'Quartic', 'Quintic', 'Rana', 'Rastrigin', 'Rosenbrock', 'Stochastic',
-           'RotatedHyperEllipsoid', 'Salomon', 'SchafferN1', 'SecondMinima',
-           'SchafferN2', 'SchafferN4', 'SchafferN6', 'Schubert', 'Periodic',
-           'Schubert3', 'Schubert4', 'SchumerSteiglitz', 'Schwefel', 'NeedleEye',
-           'Schwefel12', 'Schwefel204', 'Schwefel220', 'Schwefel221',
-           'Schwefel222', 'Schwefel223', 'Schwefel225', 'Schwefel226', 'Sargan',
-           'Sphere', 'Step', 'Step2', 'Step3', 'StepInt', 'StrechedVSineWave',
-           'StyblinskiTang', 'SumSquares', 'Trid10', 'Trid6', 'Trigonometric1',
-           'Trigonometric2', 'WWavy', 'Weierstrass', 'Whitley', 'XinSheYang1',
-           'XinSheYang2', 'XinSheYang3', 'XinSheYang4', 'Zakharov', 'HappyCat',
-           'ExpandedFiveUnevenPeakTrap', 'ExpandedEqualMinima',
-           'ExpandedDecreasingMinima', 'ModifiedVincent']
+__all__ = ['Ackley1', 'Ackley4', 'Alpine1', 'Alpine2', 'Brent', 'Brown', 'ChungReynolds',
+           'Csendes', 'Deb1', 'Deb3', 'DixonPrice', 'DropWave', 'EggHolder', 'Ellipsoid',
+           'ExpandedDecreasingMinima', 'ExpandedEqualMinima', 'ExpandedFiveUnevenPeakTrap',
+           'Exponential', 'Giunta', 'Griewank', 'HappyCat', 'HyperEllipsoid', 'KTablet',
+           'Michalewicz', 'Mishra1', 'Mishra11', 'Mishra2', 'Mishra7', 'ModifiedVincent',
+           'NeedleEye', 'Pathological', 'Periodic', 'Perm', 'Pinter', 'PowellSum',
+           'Qing', 'Quartic', 'Quintic', 'Rana', 'Rastrigin', 'Rosenbrock',
+           'RotatedHyperEllipsoid', 'Salomon', 'Sargan', 'SchafferN1', 'SchafferN2',
+           'SchafferN4', 'SchafferN6', 'Schubert', 'Schubert3', 'Schubert4',
+           'SchumerSteiglitz', 'Schwefel', 'Schwefel12', 'Schwefel204', 'Schwefel220',
+           'Schwefel221', 'Schwefel222', 'Schwefel223', 'Schwefel225', 'Schwefel226',
+           'SecondMinima', 'Sphere', 'Step', 'Step2', 'Step3', 'StepInt', 'Stochastic',
+           'StrechedVSineWave', 'StyblinskiTang', 'SumSquares', 'Trid10', 'Trid6',
+           'Trigonometric1', 'Trigonometric2', 'WWavy', 'Weierstrass', 'Whitley',
+           'XinSheYang1', 'XinSheYang2', 'XinSheYang3', 'XinSheYang4', 'Zakharov']
+
 
 
 # %% Basic function class
@@ -1676,7 +1675,7 @@ class Trigonometric2(OptimalBasic):
                 variables - 0.9))) + np.square(variables - 0.9))
 
 
-# 165 - Class W / Wavy function
+# 165 - Class W-Wavy function
 class WWavy(OptimalBasic):
     def __init__(self, variable_num):
         super().__init__(variable_num)
@@ -1684,7 +1683,7 @@ class WWavy(OptimalBasic):
         self.min_search_range = np.array([-np.pi] * self.variable_num)
         self.optimal_solution = np.array([0.] * self.variable_num)
         self.global_optimum_solution = 0.
-        self.func_name = 'W / Wavy'
+        self.func_name = 'W-Wavy'
         self.features = {'Continuous': True,
                          'Differentiable': True,
                          'Separable': True,
@@ -2027,8 +2026,3 @@ class Perm(OptimalBasic):
         return np.sum(np.square(np.sum((j.T + beta) * (
                 np.power(x, j) - np.power(1 / j.T, j)), 0)))
 
-
-# %%
-if __name__ == '__main__':
-    q = Sphere(2)
-    q.plot()

hyperheuristic.py
@@ -21,10 +21,7 @@ from os import makedirs as _create_path
 
 
 class Hyperheuristic():
-    def __init__(self, heuristic_space, problem, parameters={
-            'cardinality': 2, 'num_iterations': 100, 'num_agents': 30,
-            'num_replicas': 100, 'num_steps': 10, 'num_trials': 10},
-            file_label=''):
+    def __init__(self, heuristic_space, problem, parameters=None, file_label=''):
         # Read the heuristic space
         if isinstance(heuristic_space, list):
             self.heuristic_space = heuristic_space
@@ -36,6 +33,17 @@ class Hyperheuristic():
         else:
             raise HyperheuristicError("Invalid heuristic_space")
 
+        # Assign default values
+        if parameters is None:
+            parameters = dict(cardinality=2,        # Search operators in MHs,  lvl:0
+                              num_iterations=100,   # Iterations a MH performs, lvl:0
+                              num_agents=30,        # Agents in population,     lvl:0
+                              num_replicas=100,     # Replicas per each MH,     lvl:1
+                              num_trials=100,       # Trials per HH step,       lvl:2
+                              max_temperature=1e8,  # Initial temperature (SA), lvl:2
+                              min_temperature=1e-8,  # Threshold temp. (SA),    lvl:2
+                              cooling_rate=0.05)    # Cooling rate (SA),        lvl:2
+
         # Read the heuristic space (mandatory)
         self.problem = problem
         self.num_operators = len(self.heuristic_space)
@@ -44,9 +52,22 @@ class Hyperheuristic():
         self.parameters = parameters
         self.file_label = file_label
 
+        # Read cardinality
+        if isinstance(parameters['cardinality'], int):
+            # Fixed cardinality
+            self.cardinality_boundaries = [parameters['cardinality']] * 2
+        elif isinstance(parameters['cardinality'], list):
+            # Variable cardinality
+            if len(parameters['cardinality']) == 1:
+                self.cardinality_boundaries = [1, parameters['cardinality'][0]]
+            elif len(parameters['cardinality']) == 2:
+                self.cardinality_boundaries = parameters['cardinality'].sort()
+            else:
+                raise HyperheuristicError('Invalid cardinality!')
+
     def run(self):
         """
-        Run Random Search (integer version) to find the best metaheuristic.
+        Run Simulated Annealing to find the best metaheuristic.
         Each meatheuristic is run 'num_replicas' times to obtain statistics
         and thus its performance.
 
@@ -68,9 +89,11 @@ class Hyperheuristic():
             has performed), 'fitness', 'positions', and 'statistics'.
 
         """
-        # Create the initial solution
-        encoded_solution = np.random.randint(
-            0, self.num_operators, self.parameters['cardinality'])
+        # Create the initial solution (it is fixed to be always Random Search)
+        # It is 0 because operators._build_operators put random search as the
+        # first operator.
+        cardinality = 1
+        encoded_solution = [0]
         solution = [self.heuristic_space[index] for index in encoded_solution]
 
         # Evaluate this solution
@@ -86,49 +109,63 @@ class Hyperheuristic():
         # Save this historical register
         _save_iteration(0, historicals, self.file_label)
 
-        print('{} - perf: {}, sol: {}'.format(0, performance,
-                                              encoded_solution))
+        # Initialise the temperature and step counter
+        temperature = self.parameters['max_temperature']
+        step = 0
 
-        # Perform the random search
-        for step in range(1, self.parameters['num_steps']):
+        # Print the first status update
+        print('{} - perf: {}, sol: {}'.format(step, performance, encoded_solution))
+
+        # Perform the annealing simulation
+        while temperature > self.parameters['min_temperature']:
             # Start trials
             for trial in range(self.parameters['num_trials']):
 
-                # Select randomly a candidate solution
-                encoded_candidate_solution = np.random.randint(
-                    0, self.num_operators, self.parameters['cardinality'])
+                # Generate a neighbour cardinality
+                cardinality += np.random.randint(-1, 1)
+                if cardinality < self.cardinality_boundaries[0]:
+                    cardinality = self.cardinality_boundaries[0]
+                elif cardinality > self.cardinality_boundaries[1]:
+                    cardinality = self.cardinality_boundaries[1]
+
+                # Generate a neighbour solution
+                encoded_candidate_solution = np.random.randint(0, self.num_operators, cardinality)
                 candidate_solution = [self.heuristic_space[index]
-                                      for index in
-                                      encoded_candidate_solution]
+                                      for index in encoded_candidate_solution]
 
                 # Evaluate this candidate solution
                 candidate_performance, candidate_details =\
                     self.evaluate_metaheuristic(candidate_solution)
 
-                # Check improvement (greedy selection)
-                if candidate_performance < performance:
+                # Determine the energy (performance) change
+                delta_energy = candidate_performance - performance
+
+                # Check improvement (Metropolis criterion)
+                if (delta_energy < 0) or (np.random.rand() < np.exp(-delta_energy/temperature)):
                     encoded_solution = encoded_candidate_solution
                     solution = candidate_solution
                     performance = candidate_performance
                     details = candidate_details
 
                     # Save this historical register and break
+                    step += 1
                     _save_iteration(step, {
                         'encoded_solution': encoded_solution,
                         'solution': solution,
                         'performance': performance,
                         'details': details},
                         self.file_label)
-                    break
 
-            print('{} - perf: {}, sol: {}'.format(step, performance,
-                                                  encoded_solution))
+                    print('{} - perf: {}, sol: {}'.format(step, performance, encoded_solution))
 
-            # When zero performance is reached
-            if (performance == 0.0):
-                # Goodbye
-                break
+                    # When zero performance is reached end the simulation
+                    if performance == 0.0:
+                        return solution, performance, encoded_solution, historicals
 
+            # Update temperature
+            temperature *= 1 - self.parameters['cooling_rate']
+
+        # Return the best solution found and its details
         return solution, performance, encoded_solution, historicals
 
     def evaluate_metaheuristic(self, search_operators):

operators.py
@@ -18,6 +18,9 @@ __all__ = ['local_random_walk', 'random_search', 'random_sample',
 # -------------------------------------------------------------------------
 #    PERTURBATORS
 # -------------------------------------------------------------------------
+from numpy.core._multiarray_umath import ndarray
+
+
 def central_force_dynamic(pop, gravity=0.001, alpha=0.01, beta=1.5, dt=1.0):
     """
     Central Force Optimisation (CFO)
@@ -1052,7 +1055,7 @@ def _get_rotation_matrix(dimensions, angle=0.39269908169872414):
     # Initialise the rotation matrix
     rotation_matrix = np.eye(dimensions)
 
-    # Find the combinations without repetions
+    # Find the combinations without repetitions
     planes = list(_get_combinations(range(dimensions), 2))
 
     # Create the rotation matrix
@@ -1064,11 +1067,8 @@ def _get_rotation_matrix(dimensions, angle=0.39269908169872414):
         rotation_plane = np.eye(dimensions)
 
         # Assign corresponding values
-        rotation_plane[x, y] = np.cos(angle)
-        rotation_plane[y, y] = np.cos(angle)
-        rotation_plane[x, y] = -np.sin(angle)
-        rotation_plane[y, x] = np.sin(angle)
-
+        rotation_plane[[x, x, y, y], [x, y, x, y]] = [np.cos(angle), -np.sin(angle),
+                                                      np.sin(angle), np.cos(angle)]
         rotation_matrix = np.matmul(rotation_matrix, rotation_plane)
 
     return rotation_matrix
@@ -1118,7 +1118,7 @@ class OperatorsError(Exception):
 # GENERATOR OF SEARCH OPERATORS
 # ---------------------------------------------------------------------------
 
-def _obtain_operators(num_vals=5):
+def _obtain_operators(num_vals=11):
     """
     Generate a list of all the available search operators with a given
     number of values for each parameter (if so).
@@ -1142,13 +1142,15 @@ def _obtain_operators(num_vals=5):
                                "name_of_the_selector")
     """
     return [
+        # First line for the initial search operator
+        ("random_search", dict(scale=[1.0], distribution=["uniform"]), ["greedy"]),
         (
             "central_force_dynamic",
             dict(
                 gravity=[*np.linspace(0.0, 0.01, num_vals)],
                 alpha=[*np.linspace(0.0, 0.01, num_vals)],
-                beta=[*np.linspace(1.25, 1.75, num_vals)],
-                dt=[1.0]),
+                beta=[*np.linspace(1.00, 2.00, num_vals)],
+                dt=[*np.linspace(0.0, 2.0, num_vals)]),
             ["all", "greedy", "metropolis", "probabilistic"]),
         # (
         #    'differential_crossover',
@@ -1162,15 +1164,15 @@ def _obtain_operators(num_vals=5):
                 expression=["rand", "best", "current", "current-to-best",
                             "rand-to-best", "rand-to-best-and-current"],
                 num_rands=[1, 2, 3],
-                factor=[*np.linspace(0.5, 2.5, num_vals)]),
+                factor=[*np.linspace(0.0, 2.5, num_vals)]),
             ["all", "greedy", "metropolis", "probabilistic"]),
         (
             "firefly_dynamic",
             dict(
                 epsilon=["uniform", "gaussian", "levy"],
                 alpha=[*np.linspace(0.0, 0.5, num_vals)],
-                beta=[1.0],
-                gamma=[*np.linspace(10.0, 990.0, num_vals)]),
+                beta=[*np.linspace(0.01, 1.0, num_vals)],
+                gamma=[*np.linspace(1.0, 1000.0, num_vals)]),
             ["all", "greedy", "metropolis", "probabilistic"]),
         (
             "genetic_crossover",
@@ -1179,14 +1181,14 @@ def _obtain_operators(num_vals=5):
                          "tournament_2_100", "tournament_3_100"],
                 crossover=["single", "two", "uniform", "blend",
                            "linear_0.5_0.5"],
-                mating_pool_factor=[*np.linspace(0.1, 0.9, num_vals)]),
+                mating_pool_factor=[*np.linspace(0.01, 0.99, num_vals)]),
             ["all", "greedy", "metropolis", "probabilistic"]),
         (
             "genetic_mutation",
             dict(
-                scale=[*np.linspace(0.0, 0.9, num_vals)],
+                scale=[*np.linspace(0.01, 1.0, num_vals)],
                 elite_rate=[*np.linspace(0.0, 0.9, num_vals)],
-                mutation_rate=[*np.linspace(0.1, 0.9, num_vals)],
+                mutation_rate=[*np.linspace(0.01, 0.9, num_vals)],
                 distribution=["uniform", "gaussian", "levy"]),
             ["all", "greedy", "metropolis", "probabilistic"]),
         (
@@ -1196,23 +1198,23 @@ def _obtain_operators(num_vals=5):
                 alpha=[*np.linspace(0.0, 0.04, num_vals)]),
             ["all", "greedy", "metropolis", "probabilistic"]),
         (
-            "random_flight",
+            "random_flight",  # Particular case for Levy flight
             dict(
-                scale=[*np.linspace(0.1, 0.9, num_vals)],
+                scale=[*np.linspace(0.01, 1.0, num_vals)],
                 distribution=["levy"],
-                beta=[*np.linspace(1.25, 1.75, num_vals)]),
+                beta=[*np.linspace(1.00, 2.00, num_vals)]),
             ["all", "greedy", "metropolis", "probabilistic"]),
         (
             "random_flight",
             dict(
-                scale=[*np.linspace(0.1, 0.9, num_vals)],
+                scale=[*np.linspace(0.01, 1.0, num_vals)],
                 distribution=["uniform", "gaussian"]),
             ["all", "greedy", "metropolis", "probabilistic"]),
         (
             "local_random_walk",
             dict(
-                probability=[*np.linspace(0.1, 0.9, num_vals)],
-                scale=[*np.linspace(0.1, 0.9, num_vals)],
+                probability=[*np.linspace(0.01, 0.99, num_vals)],
+                scale=[*np.linspace(0.01, 1.0, num_vals)],
                 distribution=["uniform", "gaussian", "levy"]),
             ["all", "greedy", "metropolis", "probabilistic"]),
         (
@@ -1222,30 +1224,29 @@ def _obtain_operators(num_vals=5):
         (
             "random_search",
             dict(
-                scale=[*np.linspace(0.1, 0.9, num_vals)],
+                scale=[*np.linspace(0.01, 1.0, num_vals)],
                 distribution=["uniform", "gaussian", "levy"]),
             ["all", "greedy", "metropolis", "probabilistic"]),
         (
             "spiral_dynamic",
             dict(
-                radius=[*np.linspace(0.001, 0.99, num_vals)],
-                angle=[*np.linspace(1.0, 179.0, num_vals)],
+                radius=[*np.linspace(0.001, 0.999, num_vals)],
+                angle=[*np.linspace(0.0, 180.0, num_vals)],
                 sigma=[*np.linspace(0.0, 0.5, num_vals)]),
             ["all", "greedy", "metropolis", "probabilistic"]),
         (
             "swarm_dynamic",
             dict(
-                factor=[*np.linspace(0.1, 1.0, num_vals)],
-                self_conf=[*np.linspace(1.1, 4.1, num_vals)],
-                swarm_conf=[*np.linspace(1.1, 4.1, num_vals)],
+                factor=[*np.linspace(0.01, 1.0, num_vals)],
+                self_conf=[*np.linspace(0.01, 4.99, num_vals)],
+                swarm_conf=[*np.linspace(0.01, 4.99, num_vals)],
                 version=["inertial", "constriction"],
                 distribution=["uniform", "gaussian", "levy"]),
             ["all", "greedy", "metropolis", "probabilistic"])
         ]
 
 
-def _build_operators(heuristics=_obtain_operators(),
-                     file_name="operators_collection"):
+def _build_operators(heuristics, file_name="operators_collection"):
     """
     Create a text file containing all possible combinations of parameter
     values for each search operator.
@@ -1277,6 +1278,7 @@ def _build_operators(heuristics=_obtain_operators(),
         # Read the number of parameters and how many values have each one
         num_parameters = len(parameters)
         num_selectors = len(selectors)
+        num_combinations: int = 0
         if num_parameters > 0:
             # Read the name and possible values of parameters
             par_names = list(parameters.keys())
@@ -1286,7 +1288,7 @@ def _build_operators(heuristics=_obtain_operators(),
             par_num_values = [np.size(x) for x in par_values]
 
             # Determine the number of combinations
-            num_combinations = np.prod(par_num_values)
+            num_combinations = int(np.prod(par_num_values))
 
             # Create the table of all possible combinations (index/parameter)
             indices = [x.flatten() for x in np.meshgrid(
@@ -1375,4 +1377,4 @@ def _process_operators(simple_heuristics):
 
 
 if __name__ == '__main__':
-    _build_operators(file_name="automatic")
+    _build_operators(_obtain_operators(), file_name="automatic")