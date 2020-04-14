9d7ecd8c015490e7d4da3d566c5939674bc87386
Jorge Mario Cruz Duarte <mrcrois@icloud.com>
2020-04-14 08:56:38 -0500

    basic metaheuristics where added

--- paper1_basicmetaheuristics.py
+++ paper1_basicmetaheuristics.py
@@ -14,13 +14,14 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import tools as jt
-from scipy.stats import rankdata
+from scipy.stats import rankdata, mode
 import seaborn as sns
 import pandas as pd
 import benchmark_func as bf
 sns.set(font_scale=0.5)
 
 # Read benchmark functions and their features
+# problems = bf.__all__
 problem_features = bf.list_functions()
 
 # Read benchmark functions, their features and categorise them
@@ -29,28 +30,21 @@ categorical_features = pd.DataFrame(problem_features_without_code).T
 categories = categorical_features.groupby('Code').first()
 categories['Members'] = categorical_features.groupby('Code').count().mean(axis=1)
 
-# Read heuristic space
-with open('collections/' + 'default.txt', 'r') as operators_file:
-    heuristic_space = [eval(line.rstrip('\n')) for line in operators_file]
-search_operators = [x[0].replace('_', ' ') + "-" + "-".join(["{}".format(y) for y in [
-    *x[1].values()]]).replace('_', ' ') + "-" + x[2] for x in heuristic_space]
-
 # Read the data files
-data_frame = jt.read_json('data_files/brute-force-data.json')
+data_frame = jt.read_json('data_files/basic-metaheuristics-data.json')
 
 # Show the variable tree
 # printmsk(data_frame)
 
 # Prepare additional lists (like problems, weights, operators, and dimensions)
-# problems = list(set(data_frame['problem']))
 problems = [data_frame['problem'][index] for index in sorted(np.unique(data_frame['problem'], return_index=True)[1])]
 
-problems_categories = [problem_features[x]['Code'] for x in problems]
+problems_categories = {x: problem_features[x]['Code'] for x in problems}
 operators = (data_frame['results'][0]['operator_id'])
 dimensions = sorted(list(set(data_frame['dimensions'])))
 
 # Naive weights for tests
-operators_weights = dict()
+metaheuristics_modes = dict()
 
 # Saving images flag
 is_saving = True
@@ -70,6 +64,9 @@ plt.rc('font', family='serif', size=4)
 # Obtain matrices for problems and operators
 operator_matrix, problem_matrix = np.meshgrid(np.array(operators), np.arange(len(problems)))
 
+# 1st metaheuristic per problem+dimension
+best_metaheuristics = {prob: [] for prob in problems}
+
 # Plot a figure per dimension
 for dimension in dimensions:
     # Find indices corresponding to such a dimension
@@ -78,6 +75,7 @@ for dimension in dimensions:
     # Get fitness statistical values and stored them in 'arrays'
     temporal_array = list()
     problem_categories = list()
+    first_metaheuristics = []
 
     # Selecting data
     for dim_index in dim_indices:  # Problems
@@ -87,13 +85,25 @@ for dimension in dimensions:
                                 for op_index in range(len(operators))])
         temporal_array.append(ranked_data)
 
-    # Create the data frame
+        # Save only the best one
+        first_metaheuristics.append(np.argsort(ranked_data)[0])
+        best_metaheuristics[data_frame['problem'][dim_index]].append(np.argsort(ranked_data)[0])
+
+    # Create the data frames
     stats = pd.DataFrame(temporal_array, index=problems, columns=operators)
     stats['Group'] = pd.Series(problems_categories, index=stats.index)
+    stats_without_category = stats.sort_values(by=['Group']).drop(columns=['Group'])
+
+    best_mhs = pd.DataFrame(first_metaheuristics, index=problems, columns=['BestMH'])
+    best_mhs['Group'] = pd.Series(problems_categories, index=best_mhs.index)
+    grouped_stats = best_mhs.groupby('Group').apply(pd.DataFrame.mode).drop(columns=['Group'])
+    grouped_stats.index = [x[0] for x in grouped_stats.index]
+
+    metaheuristics_modes[dimension] = jt.df2dict(grouped_stats)
 
     # -- PART 1: PLOT THE CORRESPONDING HEATMAP --
     # Delete the Group column
-    stats_without_category = stats.sort_values(by=['Group']).drop(columns=['Group'])
+    # stats_without_category = stats.sort_values(by=['Group']).drop(columns=['Group'])
 
     # Printing section
     fig = plt.figure(figsize=(15, 10), dpi=333)
@@ -105,25 +115,10 @@ for dimension in dimensions:
     plt.show()
 
     if is_saving:
-        fig.savefig(folder_name + 'raw-heatmap-bruteforce-{}D'.format(dimension) + '.pdf', format='pdf', dpi=fig.dpi)
-
-    # -- PART 2: OBTAIN NAIVE INSIGHTS
-    grouped_stats = stats.groupby('Group').mean()
-    prop_stats = grouped_stats.div(grouped_stats.sum(axis=1), axis=0)
-
-    # Store weights in the final dictionary
-    operators_weights[dimension] = jt.df2dict(prop_stats)
-
-    fig = plt.figure(figsize=(0.08*205, 0.4*8), dpi=333)
-    ax = sns.heatmap(prop_stats, cbar=False, cmap='rainbow', robust=True, xticklabels=True, yticklabels=True)
-    plt.title("Dim: {}".format(dimension))
-    plt.yticks(rotation=0)
-    bottom, top = ax.get_ylim()
-    ax.set_ylim(bottom + 0.5, top - 0.5)
-    plt.show()
-
-    if is_saving:
-        fig.savefig(folder_name + 'cat-heatmap-bruteforce-{}D'.format(dimension) + '.pdf', format='pdf', dpi=fig.dpi)
+        fig.savefig(folder_name + 'raw-heatmap-basicmetaheuristic-{}D'.format(dimension) + '.pdf', format='pdf', dpi=fig.dpi)
 
 # Save weight for the benchmark problems contained in brute-force-data.json and default.txt
-jt.save_json(operators_weights, 'data_files/operators_weights')
+for key, val in best_metaheuristics.items():
+    best_metaheuristics[key] = np.array(val)
+jt.save_json(best_metaheuristics, 'data_files/best_basicmetaheuristics')
+jt.save_json(metaheuristics_modes, 'data_files/modes_metaheuristics')
