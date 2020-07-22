6a74a938622d306ced19da93041e4f9102f6a372
Jorge Mario Cruz Duarte <mrcrois@icloud.com>
2020-07-22 17:21:15 -0500

    almost finished  2

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='customhys',
+    version='1.0.0',
+    packages=setuptools.find_packages(),
+    url='https://github.com/jcrvz/customhys',
+    license='MIT License',
+    author='Jorge Mario Cruz-Duarte',
+    author_email='jorge.cruz@tec.mx',
+    description='This framework provides tools for solving, but not limited to, continuous optimisation problems '
+                'using a hyper-heuristic approach for customising metaheuristics. '
+)
