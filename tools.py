74e140d37681f849b85047128f747a60af6629ad
Jorge Mario Cruz Duarte <mrcrois@icloud.com>
2021-05-20 11:12:54 -0500

    tools update

--- tools.py
+++ tools.py
@@ -214,7 +214,7 @@ def preprocess_files(main_folder='data_files/raw/', kind='brute_force', only_las
             label_operator = 'step'
             # Initialise iteration data
             file_data = {'step': list(), 'performance': list(), 'statistics': list(),
-                         'encoded_solution': list(), 'hist_fitness': list()}
+                         'encoded_solution': list(), 'hist_fitness': list(), 'hist_positions': list()}
 
         # Walk on the subfolder's files
         for file_name in tqdm(file_names, desc='{} {}, last={}'.format(
@@ -244,8 +244,10 @@ def preprocess_files(main_folder='data_files/raw/', kind='brute_force', only_las
                 # Only save the historical fitness values when operator_id is the largest one
                 if only_laststep and operator_id == last_step:
                     file_data['hist_fitness'] = [x['fitness'] for x in temporal_data['details']['historical']]
+                    file_data['hist_positions'] = [x['positions'] for x in temporal_data['details']['historical']]
                 else:
                     file_data['hist_fitness'].append([x['fitness'] for x in temporal_data['details']['historical']])
+                    file_data['hist_positions'].append([x['positions'] for x in temporal_data['details']['historical']])
 
             # Following information can be included but resulting files will be larger
             # file_data['fitness'].append(temporal_data['details']['fitness'])
