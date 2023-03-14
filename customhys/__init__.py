095f73caa7b135bed7b7f78397730b8957b53187
jose-tapia <jose.tapiaav@gmail.com>
2023-03-14 04:10:15 -0600

    init?

--- customhys/__init__.py
+++ customhys/__init__.py
@@ -1,11 +1,3 @@
 __all__ = ['benchmark_func', 'experiment', 'hyperheuristic', 'metaheuristic', 'operators', 'population', 'tools']
 # Under revision: 'characterisation', 'visualisation'
 __version__ = "1.1"
-
-import benchmark_func
-import population
-import operators
-import tools
-import metaheuristic
-import hyperheuristic
-import experiment
